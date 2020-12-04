b3923e29c148f995df78f819eecab2acdea05a4e
Carrie Pascale <cpascale43@gmail.com>
2020-12-04 16:19:11 -0500

    solved apple box

--- exercises/apple-box/index.js
+++ exercises/apple-box/index.js
@@ -1,9 +1,9 @@
 // --- Directions
-// There is a box with a capacity of 5000 grams. The box may already contain some items 
-// reducing its capacity. You'll be adding apples to the box until it's full. 
-// Given a zero indexed array A consisting of N integers, representing the weight 
-// of items already in the box and each apple's weight, return the maximum number of 
-// apples that could fit in the box. 
+// There is a box with a capacity of 5000 grams. The box may already contain some items
+// reducing its capacity. You'll be adding apples to the box until it's full.
+// Given a zero indexed array A consisting of N integers, representing the weight
+// of items already in the box and each apple's weight, return the maximum number of
+// apples that could fit in the box.
 // A contains between 1 and 100 applies and the numbers are between 0 and 5000.
 // A consists of K, the first element representing the sum of the weight of the items
 // already in the box followed by 0 or more integers representing individual apple weights.
@@ -12,6 +12,24 @@
 // [4650, 150, 150, 150] => 2 (two apples of weight 150g each)
 // [4850, 100, 30, 30, 100, 50, 100] => 3 (two 30g apples and one 50g)
 
-function appleBox(A){}
+function appleBox(A) {
+  const capacity = 5000;
+  let currentWeight = A[0];
+  let apples = A.slice(1);
+  let numOfApples = 0;
 
-module.exports = appleBox
+  // sort apples from lightest to heaviest
+  apples = apples.sort((a, b) => a - b);
+  let lightest = 0;
+
+  // add from the front to add max # of apples
+  while (currentWeight <= capacity) {
+    currentWeight += apples[lightest];
+    lightest++;
+    numOfApples++;
+  }
+
+  return numOfApples - 1;
+}
+
+module.exports = appleBox;
--- exercises/apple-box/test.js
+++ exercises/apple-box/test.js
@@ -9,7 +9,7 @@ test("appleBox works on an array with 4650 grams", () => {
   expect(appleBox(A)).toEqual(2);
 });
 
-test("appleBox works on an array with 4650 grams", () => {
+test("appleBox works on an array with 4850 grams", () => {
   let A = [4850, 100, 30, 30, 100, 50, 100];
   expect(appleBox(A)).toEqual(3);
 });
