1e6ab49e5ccf9214b32f746f0bb09d1d0ea82c60
Carrie Pascale <cpascale43@gmail.com>
2020-12-08 20:12:27 -0500

    solved string search

--- exercises/string-search/index.js
+++ exercises/string-search/index.js
@@ -7,7 +7,16 @@
 // 'indexOf('oox', 'ooboxoooxo') => 6
 
 function indexOf(str1, str2) {
+  let end = str1.length;
 
+  for (let i = 0; i < str2.length; i++) {
+    if (str2.slice(i, end) === str1) {
+      return i;
+    }
+    end++;
+  }
+
+  return -1;
 }
 
-module.exports = indexOf
+module.exports = indexOf;
